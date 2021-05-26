6cc754fcef609f0b65acfb6e8a7c85ce68f919a9
bruno pinho <iaishowcs@gmail.com>
2021-05-26 10:26:49 -0300

    OpenApi documentation

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -2,6 +2,11 @@ package com.project.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class BootcampApplication {
@@ -10,9 +15,15 @@ public class BootcampApplication {
 		SpringApplication.run(BootcampApplication.class, args);
 	}
 
-
-	
-
-	
+		// Configurando o OpenAPI
+		@Bean // Considerar ela para subir
+		public OpenAPI customOpenAPI() {
+			return new OpenAPI()
+			.info(new Info()
+			.title("")
+			.version("1.0")
+			.termsOfService("http://swagger.io/terms")
+			.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+		}
 
 }
