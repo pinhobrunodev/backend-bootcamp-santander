2759a2c38295aa66b61a30e48834f313cf72b5a4
bruno pinho <iaishowcs@gmail.com>
2021-05-27 14:56:44 -0300

    title swagger app

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -20,7 +20,7 @@ public class BootcampApplication {
 		public OpenAPI customOpenAPI() {
 			return new OpenAPI()
 			.info(new Info()
-			.title("")
+			.title("Projeto criado para o bootcamp DIO - Santander")
 			.version("1.0")
 			.termsOfService("http://swagger.io/terms")
 			.license(new License().name("Apache 2.0").url("http://springdoc.org")));
